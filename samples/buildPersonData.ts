7b88b414792a8ff63fbf69e8bb36c91cea10b845
Jez Norton <jeznorton@hotmail.co.uk>
2016-12-17 15:56:57 +0000

    Introducing sytemjs & module import export

--- samples/buildPersonData.ts
+++ samples/buildPersonData.ts
@@ -1,29 +1,6 @@
 
-
-// function buildPersonData(personData:any) {
-//   return `${personData.firstName} ${personData.lastName} ${personData.address}`;
-// }
-
-// function buildPersonData({firstName, lastName}, address = ['']) {
-//   return `${firstName} ${lastName} ${address}`;
-// }
-
-function buildPersonData({firstName, lastName}, ...address) {
+export function buildPersonData({firstName, lastName}, ...address) {
   return `${firstName} ${lastName} ${address}`;
 }
 
-const partialAddress = ["Sedbrook Farm", "Litton Lane", "Hinton Blewett"];
-
-const firstName = 'Jeremy',
-  lastName = 'Norton',
-  address = 'Sedbrook Farm';
-const personData = {
-  firstName,
-  lastName
-};
-
-// const address2 = partialAddress.concat(['BS39 5AY'])
-const address2 = [...partialAddress, 'BS39 5AY']
-
-console.log(buildPersonData(personData, address2));
 
--- samples/hello-world.ts
+++ samples/hello-world.ts
@@ -1,27 +1,17 @@
-class Startup {
-    public static main(): number {
-        console.log('Hello World');
-        return 0;
-    }
-}
-const message = 'Hello World';
-const sayHi = (message: string) => {
-    console.log(message);
-}
-Startup.main();
-sayHi(message);
 
-class Person {
-    name: string;
-    constructor(name: string) {
-        this.name = name;
-    }
-    sayHi = function () {
-        setTimeout(() => {
-            console.log("Hi, my name is " + this.name);
-        }, 1000);
-    }
-}
-const personName: string = "Bob";
-var aPerson = new Person(personName);
-aPerson.sayHi();
+const partialAddress = ["Sedbrook Farm", "Litton Lane", "Hinton Blewett"];
+import { buildPersonData } from './buildPersonData';
+
+const firstName = 'Jeremy',
+    lastName = 'Norton',
+    address = 'Sedbrook Farm';
+const personData = {
+    firstName,
+    lastName
+};
+
+// const address2 = partialAddress.concat(['BS39 5AY'])
+const address2 = [...partialAddress, 'BS39 5AY']
+
+console.log(buildPersonData(personData, address2));
+
