d540d18b38cf3ed0c7d5117aa1818b50a6ff0944
Jez Norton <jeznorton@hotmail.co.uk>
2016-12-18 17:54:43 +0000

    Started applying error handling

--- server/api/apiGetAllCourses.ts
+++ server/api/apiGetAllCourses.ts
@@ -3,10 +3,11 @@ import { Request, Response } from "express";
 import { findAllCourses } from "../queries/findAllCourses";
 
 export function apiGetAllCourses(req: Request, res: Response) {
-
+    // throw new Error("Error occurred");
     findAllCourses()
+        .then(() => { throw new Error('Error'); })
         .then(results => {
             res.status(200).json({ results });
-        });
+        }).catch(err => res.status(500).send());
 
 }
--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,14 @@
 
 import { findAllCourses } from './queries/findAllCourses';
-
-
+import { initRestApi } from './api/api';
+import { apiErrorHandler } from './api/apiErrorHandler';
 import * as express from 'express';
 import { Application } from 'express';
 const app: Application = express();
 
-app.route('/api/courses').get((req, res) => {
-    findAllCourses()
-        .then(results => {
-            res.status(200).json({ results });
-        });
-})
+initRestApi(app);
+
+app.use(apiErrorHandler);
 app.listen(8091, () => {
     console.log('Server is running  on port 8091 ... ')
 });
